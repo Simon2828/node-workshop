e9879fd34171f4d7dcba26acb4b23a87e3b93ea9
simon2828 <simon.tanner28@gmail.com>
2017-05-29 17:20:26 +0100

    first commit

--- workshop.js
+++ workshop.js
@@ -5,15 +5,40 @@ function getDistance(pos1, pos2) {
     return Math.sqrt(Math.pow(pos1.lat - pos2.lat, 2) + Math.pow(pos1.lng - pos2.lng, 2));
 }
 
+// function getIssPosition() {
+//     return request("http://api.open-notify.org/iss-now.json")
+//         .then(
+//             function(response) {
+//                 // Parse as JSON
+//                 response = JSON.parse(response);
+//                 // Return object with lat and lng
+//                 return {
+//                     lat: response.iss_position.latitude,
+//                     long: response.iss_position.longitude
+//                 }
+//             }
+//         )
+// }
+
 function getIssPosition() {
-    return request(/* ... */)
+    return request('http://api.open-notify.org/iss-now.json')
         .then(
-            function(responce) {
-                // Parse as JSON
-                // Return object with lat and lng
+            function(res) {
+                res=JSON.parse(res);
+                return {
+                    lat: res.iss_position.latitude,
+                    lng: res.iss_position.longitude
+                };
             }
         )
+        .then(
+            function(latLong){
+                console.log('latLong', latLong);
+                return;
+            })
 }
+
+getIssPosition();
 
 function getAddressPosition(address) {
 
